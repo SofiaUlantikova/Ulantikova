edc7048ea6aedd4790975f7cf46bcaf2f022b9cc
Sofia Ulantikova <sonlantik@gmail.com>
2024-05-14 17:42:30 +0300

    analised data

--- task_4_DAC/8-graph.py
+++ task_4_DAC/8-graph.py
@@ -3,7 +3,11 @@ import numpy as np
 from math import ceil
 
 with open('settings.txt', 'r') as f:
-    data = [float(line) for line in f.read().split('\n')]
+    lines = f.read().split('\n')
+    s = [4, 3]
+    for line in lines:
+        print(line)
+    data = [float(lines[i].split()[s[i]]) for i in range(len(lines))]
 
 freq = data[0]
 
@@ -12,8 +16,6 @@ period = 1/freq
 step = data[1]
 
 voltage = np.loadtxt('data.txt', dtype=float)
-
-voltage *= 3.3/256
 
 timepoints = np.linspace(0, len(voltage)*period, len(voltage))
 
@@ -49,8 +51,10 @@ plt.legend(loc=1)
 
 plt.title(label='Процесс заряда и разряда конденсатора в RC-цепи', loc='center', wrap=True)
 
-plt.text(0.55*timepoints[-1], 0.8*np.max(voltage), f'Время зарядки = {time_charged:4.1f} с', wrap=True)
+plt.text(0.6*timepoints[-1], 0.8*np.max(voltage), f'Время зарядки = {time_charged:4.1f} с', wrap=True)
 
 plt.text(0.6*timepoints[-1], 0.65*np.max(voltage), f'Время разрядки = {time_discharged:4.1f} с', wrap=True)
 
 plt.show()
+
+plt.savefig('/home/b04-304/Ulantikova/task_4_DAC/graphic.svg')
